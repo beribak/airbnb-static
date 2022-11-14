21a831e0d26f385c2555382e4632aa4960ac7a2a
danko beribak <beribakdanko@gmail.com>
2022-11-14 20:05:23 +0100

    first commit

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,16 @@
+require "open-uri"
+
+class FlatsController < ApplicationController
+    def index
+        url = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json"
+        @flats = JSON.parse(URI.open(url).read)
+    end
+
+    def show 
+        url = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json"
+        @flats = JSON.parse(URI.open(url).read)
+        @id = params[:id].to_i
+        @flat = @flats.select { |flat| flat['id'] == @id }
+    end
+end
+
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "flats#index"
+  get "/flats/:id", to: "flats#show", as: :flat
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
